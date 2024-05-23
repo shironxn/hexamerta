4b6f9a7cb22a02d2879a227e7a6b5d20188fad51
shironxn <qshiro69@gmail.com>
2024-05-23 22:50:46 +0700

    chore: auth redirect url

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -3,6 +3,18 @@
 import { redirect } from "next/navigation";
 import { createClient } from "../lib/supabase/server";
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.includes("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  return url;
+};
+
 export async function signIn() {
   const supabase = createClient();
 
@@ -14,7 +26,7 @@ export async function signIn() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: process.env.BASE_URL + "/auth/callback?next=/acara",
+      redirectTo: getURL() + "auth/callback?next=/acara",
     },
   });
 
