b62d2bcecdea0d057f098fd863b5eb9dbed5ddb5
shironxn <qshiro69@gmail.com>
2024-06-12 09:40:02 +0700

    chore: laper bet

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
     <div className="h-screen flex flex-col items-center justify-center prose text-center w-full">
       <h1>404</h1>
       <p className="max-w-md">
-        Don't worry, our team is already on it. Please try refreshing the page
-        or come back later.
+        Dont worry, our team is already on it. Please try refreshing the page or
+        come back later.
       </p>
       <Link href="/" className="btn btn-primary">
         Return Home
