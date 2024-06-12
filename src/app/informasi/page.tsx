969b7cc2c4c0011eac315c66554e1532c4da084e
shironxn <qshiro69@gmail.com>
2024-06-12 20:22:00 +0700

    chore: ilyas gay

--- src/app/informasi/page.tsx
+++ src/app/informasi/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <div className="space-y-8 max-md:mt-20 min-h-screen md:hero max-w-lg flex items-center prose">
-      <h1>ilyas gay</h1>
+      <h2>
+        Untuk informasi lebih lanjut silahkan kunjungin instagram @hexamerta.jpg
+      </h2>
     </div>
   );
 }
